bdfd779817d3369115e2f1b8aca6ea7577a51802
Roger Esteve <Coconaut777@rocketmail.com>
2017-05-15 23:26:51 +0200

    Main page done
    - Button to users list
    - Button to create user

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ const BrowserWindow = electron.BrowserWindow;
 
 app.on("ready", function() {
   let w = new BrowserWindow({
-    width: 640,
-    height: 480,
+    width: 640*2,
+    height: 480*2,
+    minWidth: 850,
+    minHeight: 600,
     frame: true
   });
+  w.setMenu(null);
+  w.loadURL("file:///" + __dirname + "/index.html");
+  exports.window = w;
 });
